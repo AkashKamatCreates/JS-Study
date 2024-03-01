dc7806bc89c044404a9d2e9a0041386b9988b6da
AkashKamatCreates <kamatakash13@gmail.com>
2024-03-01 22:22:11 +0530

    commit message

---
+++ 04_controlflow/one.js
@@ -0,0 +1,12 @@
+const loginstatus = true;
+
+if(loginstatus){
+    console.log('user is logged in')
+}else{
+    console.log('user is not logged in');
+}
+
+const creditcard = true;
+if(loginstatus && creditcard){
+    console.log("STATUS: allowed to buy course.")
+}
