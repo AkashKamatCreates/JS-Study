d57291ea5b6674f530cb949117684eec93c34ddc
AkashKamatCreates <kamatakash13@gmail.com>
2024-03-01 22:44:03 +0530

    m

---
+++ 04_controlflow/truth.js
@@ -0,0 +1,23 @@
+const useremail = "akash@google.com"
+
+if(useremail)console.log("got user email")
+
+
+let val1;
+val1 = 5 ?? 10
+val1 = null ?? 103
+val1 = undefined ?? 234
+console.log(val1);
+
+//?? is a null safety operator
+
+/**
+ * ternary operator ?
+ */
+
+// condition? true:false
+// 
+
+const iceteaprice = 199;
+iceteaprice>=150 ? console.log("ice tea is more than 150") : console.log("ice tea is cheap")
+
