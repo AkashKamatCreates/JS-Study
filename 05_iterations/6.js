8260c7c6a3a312ffa3f797545c25a90c08ffc083
AkashKamatCreates <kamatakash13@gmail.com>
2024-03-01 23:57:35 +0530

    m

---
+++ 05_iterations/6.js
@@ -0,0 +1,18 @@
+// const superheroNames = [
+//     "Superman",
+//     "Batman",
+//     "Wonder Woman",
+//     "Spider-Man",
+//     "Iron Man",
+//     "Captain America",
+//     "Thor",
+//     "Black Widow",
+//     "Hulk",
+//     "Flash"];
+
+// superheroNames.forEach(element => {
+//     console.log(element)
+// });
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const newnums = nums.filter((nums)=>nums>4){}
