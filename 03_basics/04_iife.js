321f25d003c9e45a6815fb9bc1c6b500af2a33b1
Akash Kamat <100783004+AkashKamatCreates@users.noreply.github.com>
2024-03-01 14:00:20 +0000

    asd

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,1 +1,19 @@
 //Immediately invoked Function Expressions IIFE
+//IIFE IS PUTTING PARENTHESIS AROUND FUNCITON FOR ITS IMMEDIATE 
+//EXECUTION WITHOUT A SEPARETE CALL
+(function message(){ //named iife
+    console.log("DBCONNECTED")
+})(); //semi colon is necessary to end an iife funciton compulsory.
+
+//arrow funciton in iife:
+(()=>{ // nameless iife
+    console.log("hello from iife")
+})();
+
+
+((name)=>{
+    console.log(`${name} hello from iife`);
+})("akash kamat");
+
+
+
