dc90d389010efc5231600caa729f77e9e9f527e8
Akash Kamat <100783004+AkashKamatCreates@users.noreply.github.com>
2024-03-01 12:58:12 +0000

    jhv

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -2,7 +2,22 @@ let a = 10;
 var b = 20;
 const c = 30;
 
-console.log(a)
-console.log(b)
-console.log(c)
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+function one(){
+    const username = "akash kamat"
+    function two(){
+        const websites = "google.com"
+        console.log(username)
+    }
+    // console.log(website);
+    //parent can access child but vice versa is not true.
+    two() 
+}
+one()
+
+//same parent child relations applicable to nested for, nested if else loops
+
 
