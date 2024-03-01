2cf675cabad9285a29a644890eecec8ecc11dbbc
AkashKamatCreates <kamatakash13@gmail.com>
2024-03-01 23:10:25 +0530

    night commit vid 29

---
+++ 05_iterations/one.js
@@ -0,0 +1,7 @@
+for (let index = 0; index <= 20; index++) {
+    if(index==5){
+        console.log("5 detected!!!!")
+        continue;
+    }
+    console.log("current index is", index)    
+}
---
+++ 05_iterations/otherloops.js
@@ -0,0 +1,7 @@
+//for of loop
+
+//this is just like foreach iterator in java
+const arr = [1, 2, 3, 4, 5]
+for (const num of arr) {
+    console.log(num)
+}
